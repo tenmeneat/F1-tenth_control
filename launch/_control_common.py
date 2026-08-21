e180bbc1ebcbec6d6062efb35bfb2b1d5e47e4d1
tenmeneat <tenmeneat@github.com>
2026-08-22 07:03:24 +0900

    sync: 08/22

--- launch/_control_common.py
+++ launch/_control_common.py
@@ -210,7 +210,7 @@ def declare_common_args(
                         '구 이름 l1_gain'
         ),
         DeclareLaunchArgument(
-            'l1_speed_gain', default_value='0.35',
+            'l1_speed_gain', default_value='0.32',
             description='L1 룩어헤드 거리의 **속도 계수** [s] (공식: l1_offset + v*l1_speed_gain). '
                         '구 이름 l1_distance'
         ),
